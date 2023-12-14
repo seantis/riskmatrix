a7a054e113ab5ccfdbeff424aeecfd6c38fd9209
David Salvisberg <david.salvisberg@seantis.ch>
2023-12-14 11:14:04 +0100

    mypy: Adds some additional stubs and cleans up obsolete overrides

--- stubtest.sh
+++ stubtest.sh
@@ -9,14 +9,17 @@ if [[ -z "${VIRTUAL_ENV:-}" && "${SKIP_VENV:-}" -ne "1" ]]; then
     source venv/bin/activate
 fi
 
-echo "Running stubtest on fanstatic"
-stubtest fanstatic \
-         --mypy-config-file pyproject.toml \
-         --allowlist tests/stubtest/fanstatic_allowlist.txt \
-
 # the pyramid stubs are incomplete so we ignore missing stub
 echo "Running stubtest on pyramid"
 stubtest pyramid \
          --mypy-config-file pyproject.toml \
          --allowlist tests/stubtest/pyramid_allowlist.txt \
          --ignore-missing-stub
+
+echo "Running stubtest on transaction"
+stubtest transaction --mypy-config-file pyproject.toml \
+                     --allowlist tests/stubtest/transaction_allowlist.txt
+
+echo "Running stubtest on zope.sqlalchemy"
+stubtest zope.sqlalchemy --mypy-config-file pyproject.toml \
+                         --allowlist tests/stubtest/zope.sqlalchemy_allowlist.txt
